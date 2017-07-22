af25f3d6dc69fe1ba4509e23020a042252e9ab7a
Lucas Silva Guimarães <lucas.rv@hotmail.com>
2017-07-22 02:29:20 -0300

    Controle de semestre e classe de enumeradores

---
+++ PblSolution/Pbl/Models/Enumeradores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pbl.Models
+{
+    public class Enumeradores
+    {
+
+        public enum TipoUsuario
+        {
+            Diretor = 1,
+            Professor = 2,
+            Aluno = 3
+        }
+
+    }
+}
